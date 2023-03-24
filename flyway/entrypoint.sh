74324664cb4015a602ae1537c7a510483347e4c7
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2023-03-24 12:36:14 +0100

    Fix password and db name

--- flyway/entrypoint.sh
+++ flyway/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get postgresql database version and trim whitespaces
-DATABASE_VERSION=$(PGPASSWORD=password psql --tuples-only -U postgres -h postgres.web3signer.dappnode -p 5432 -d web3signer -c "SELECT version FROM database_version WHERE id=1;" | awk '{print $1}' | tr -d '[:space:]')
+DATABASE_VERSION=$(PGPASSWORD=mainnet psql --tuples-only -U postgres -h postgres.web3signer.dappnode -p 5432 -d web3signer-mainnet -c "SELECT version FROM database_version WHERE id=1;" | awk '{print $1}' | tr -d '[:space:]')
 
 # Get the latest migration file version
 LATEST_MIGRATION_VERSION=$(ls -1 /flyway/sql/ | tail -n 1 | cut -d'_' -f1 | cut -d'V' -f2 | sed 's/^0*//')
@@ -19,7 +19,7 @@ else
   echo "Database version is less than the latest migration file version. Migrating..."
   echo "Database version: $DATABASE_VERSION"
   echo "Latest migration file version: $LATEST_MIGRATION_VERSION"
-  flyway -baselineOnMigrate="true" -baselineVersion="${DATABASE_VERSION}" -url=jdbc:postgresql://postgres.web3signer.dappnode:5432/web3signer -user=postgres -password=password -connectRetries=60 migrate
+  flyway -baselineOnMigrate="true" -baselineVersion="${DATABASE_VERSION}" -url=jdbc:postgresql://postgres.web3signer.dappnode:5432/web3signer-mainnet -user=postgres -password=mainnet -connectRetries=60 migrate
   echo "Migration completed"
   exit 0
 fi
