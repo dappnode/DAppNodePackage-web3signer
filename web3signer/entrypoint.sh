aff43c1c88b6d188f04eca805d8cca5615c01582
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-02-06 21:57:28 +0100

    Add http:// to cors alow list
    Add http:// to cors alow list

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -61,7 +61,7 @@ exec /opt/web3signer/bin/web3signer \
   --http-listen-port=9000 \
   --http-listen-host=0.0.0.0 \
   --http-host-allowlist="web3signer.web3signer.dappnode,brain.web3signer.dappnode,prysm.migration.dappnode,$ETH2_CLIENT_DNS" \
-  --http-cors-origins="web3signer.web3signer.dappnode,brain.web3signer.dappnode,prysm.migration.dappnode,$ETH2_CLIENT_DNS" \
+  --http-cors-origins="http://web3signer.web3signer.dappnode,http://brain.web3signer.dappnode,http://prysm.migration.dappnode,http://$ETH2_CLIENT_DNS" \
   --metrics-enabled=true \
   --metrics-host 0.0.0.0 \
   --metrics-port 9091 \
